9c5e8cf8514b43c5fedeb7f5875d6703e79dde5a
K0ron <kevin.caron.91@gmail.com>
2026-01-28 18:33:07 +0100

    create skeleton's project and add run file

---
+++ src/main.cpp
@@ -0,0 +1,9 @@
+#include <sterilizer.h>
+#include <iostream>
+using namespace std;
+
+int main() {
+    Sterilizer sterilizer;
+    cout << "Squelette Sterilizer créé !" << endl;
+    return 0;
+}
---
+++ src/sterilizer.h
@@ -0,0 +1,54 @@
+#ifndef STERILIZER_H
+#define STERILIZER_H
+
+#include <chrono>
+using namespace std;
+
+
+enum class State {
+    IDLE,
+    HEATING,
+    HOLD,
+    FINISHED
+};
+
+struct Configuration {
+    float targetTemperature;
+    int durationSeconds; 
+};
+
+// Main class representing the sterilizer device
+class Sterilizer {
+public: 
+    Sterilizer();
+
+    //Public interface
+    void configure(float temperature, int durationSec);
+    void start();
+    void stop();
+    void update(); // Called in loop to simulate heating
+
+    //Getters
+    float getTemperature() const;
+    State getState() const; 
+    int getRemainingTime() const;
+
+private: 
+    Configuration config; 
+    State currentState;
+
+    float currentTemperature;
+    bool heatingActive; 
+
+    bool timerActive;
+    chrono::steady_clock::time_point timerStart;
+
+};
+
+#endif // STERILIZER_H
+
+
+
+
+
+
